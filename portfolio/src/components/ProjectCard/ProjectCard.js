74e020780a3ba58402eb4b6b3cb309df599b8802
cserna98 <100805051+cserna98@users.noreply.github.com>
2023-01-02 23:32:44 -0500

    barra desplegable componente project

--- portfolio/src/components/ProjectCard/ProjectCard.js
+++ portfolio/src/components/ProjectCard/ProjectCard.js
@@ -20,10 +20,13 @@ const formattedpushed_at = pushed_at.toLocaleDateString('es-ES', options);
 
   return (
     <div className="project">
-    <h3>{Project.name}</h3>
-    <p>language: {Project.language}</p>
-    <p>Creation date: {formattedcreated_at}</p>
-    <p>Change date: {formattedpushed_at}</p>            
+      <h3>{Project.name}</h3>      
+        <div>
+          <p>language: {Project.language}</p>
+          <p>Creation date: {formattedcreated_at}</p>
+          <p>Change date: {formattedpushed_at}</p> 
+        </div>
+          
   </div> 
   );
 }
--- portfolio/src/components/routes/Projects.js
+++ portfolio/src/components/routes/Projects.js
@@ -1,9 +1,14 @@
 import { useEffect,useState } from 'react';
 import ProjectCard from '../ProjectCard/ProjectCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+
 
 
 function Projects() {
 const [repos,SetRepos] = useState([]);
+const [isVisible, setIsVisible] = useState(false)
+const [isVisible2, setIsVisible2] = useState(false)
 
   async function fetchData() {
     const response = await fetch('https://api.github.com/users/cserna98/repos');
@@ -11,8 +16,7 @@ const [repos,SetRepos] = useState([]);
     const newdata = data.map(({  id, url, language, name, created_at, pushed_at }) => ({ id, url, language, name, created_at, pushed_at }));
     SetRepos(()=>newdata)
     console.log(repos)  
-  }
-  
+  }  
   useEffect(() => {
     fetchData(); 
   }, []);
@@ -20,31 +24,44 @@ const [repos,SetRepos] = useState([]);
   console.log(repos)  
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
-
-
     return (
       <main className='colum Disp_flex' >
         <h2>PROJECTS</h2>
-        <div  className="Disp_flex projects">                
-        <div className="Disp_flex projectState">
-          <h2>working on</h2>
-          <div className='Disp_flex project_cards'>
-          {repos.map((e)=>(
-            <ProjectCard key={e.id} Project={e}/>
-          ))} 
-          </div>             
-        </div>
+      
+        <div  className="Disp_flex projects"> 
+
+          <div className={`Disp_flex projectState ${isVisible ? 'visible colum' : 'hidden'}`}>
+            <h2>working on</h2>                
+              {isVisible && (
+                <div className='Disp_flex project_cards'>
+                  {repos.map((e)=>(
+                    <ProjectCard key={e.id} Project={e}/>
+                  ))}
+                </div> 
+              )}
+            <button 
+            onClick={() => setIsVisible(!isVisible)}
+            className={`btn ${isVisible ? 'btn-up' : 'btn-down'}`}>
+              <FontAwesomeIcon icon={faCaretDown}  />
+            </button>                      
+          </div>
 
 
-        <div className="Disp_flex projectState">
-          <h2>finalized</h2>
-          <div className='Disp_flex project_cards'>
-          {repos.map((e)=>(
-            <ProjectCard key={e.id} Project={e}/>                     
-          ))}            
-          </div>             
-        </div>     
-
+          <div className={`Disp_flex projectState ${isVisible2 ? 'visible colum' : 'hidden'}`} >
+            <h2>finalized</h2>           
+              {isVisible2 && (
+                <div className='Disp_flex project_cards'>
+                  {repos.map((e)=>(
+                    <ProjectCard key={e.id} Project={e}/>
+                  ))} 
+                </div> 
+              )}
+            <button 
+            onClick={() => setIsVisible2(!isVisible2)}
+            className={`btn ${isVisible2 ? 'btn-up' : 'btn-down'}`}>
+              <FontAwesomeIcon icon={faCaretDown}  />
+            </button>
+          </div>
         </div>           
       </main>
     );
