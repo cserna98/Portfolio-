076cf7e32841e563e44337e33a9621f8d5673b05
cserna98 <100805051+cserna98@users.noreply.github.com>
2023-01-03 01:15:15 -0500

    Escogiendo colores proncipales

--- portfolio/src/components/routes/Home.js
+++ portfolio/src/components/routes/Home.js
@@ -1,7 +1,8 @@
+import profilePicture from '../../assets/images/profile.jpg'
 function Home() {
 
     return (
-      <main>
+      <main className='Disp_flex' id='home'>
         <div className="description Disp_flex">
         <h1>Hello, i'm Cristian and this is my personal web site </h1>
         <p className="descriptionText">Web developer in training, with a strong interest
@@ -12,6 +13,9 @@ function Home() {
         society. I am constantly striving to improve and 
         enhance my skills in order to provide value and make
         a difference in the world.</p>        
+      </div>
+      <div className="profilePicture">
+        <img src={profilePicture} ></img>
       </div>
       </main>
       
