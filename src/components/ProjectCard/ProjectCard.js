8acf8e6df5dd7e028ae01c459997a1f88c85d6c3
Cristian <criserlo98@gmail.com>
2023-02-26 00:04:40 -0500

    merge objeto de repos

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './ProjectCard.module.css';
 import { useEffect,useState } from 'react';
+import { Link } from "react-router-dom"
 
 function ProjectCard({Project}) {
 
 const created_at = new Date(Project.created_at);
 const pushed_at = new Date(Project.pushed_at);
 
+console.log(Project.created_at)
 
 const options = {
   year: 'numeric',
@@ -24,8 +26,11 @@ const formattedpushed_at = pushed_at.toLocaleDateString('es-ES', options);
       <h3>{Project.name}</h3>      
         <div>
           <p>language: {Project.language}</p>
+          <p>Repositorio:<Link to={Project.svn_url}>{Project.svn_url}</Link> </p>
+          <p>Pagina: {Project.urlPage}</p>
           <p>Creation date: {formattedcreated_at}</p>
           <p>Change date: {formattedpushed_at}</p> 
+
         </div>
           
   </div> 
--- src/components/routes/Projects.js
+++ src/components/routes/Projects.js
@@ -5,18 +5,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { GlobalContext } from '../utils/global.contex';
 import styles from './Projects.module.css';
+import reposdata from '../../assets/text/repos.json' 
+
 
 
 function Projects() {
 const {language} = GlobalContext()  
 const [repos,SetRepos] = useState([]);
+const [mergerepos, setMergeRepos] = useState()
 const [isVisible, setIsVisible] = useState(false)
 const [isVisible2, setIsVisible2] = useState(false)
+
+
 
   async function fetchData() {
     const response = await fetch('https://api.github.com/users/cserna98/repos');
     const data = await response.json();   
-    const newdata = data.map(({  id, url, language, name, created_at, pushed_at }) => ({ id, url, language, name, created_at, pushed_at }));
+    const newdata = data.map(({  id, svn_url, language, name, created_at, pushed_at }) => ({ id, svn_url, language, name, created_at, pushed_at }));
     SetRepos(()=>newdata)
     console.log(repos)  
   }  
@@ -24,7 +29,17 @@ const [isVisible2, setIsVisible2] = useState(false)
     fetchData(); 
   }, []);
 
-  console.log(repos)  
+  useEffect(()=>{
+    const mergedArray = repos.map(obj1 => {
+      const obj2 = reposdata.find(obj2 => obj1.name === obj2.name);
+      return obj2 ? { ...obj1, ...obj2 } : obj1;
+    });
+
+    setMergeRepos(mergedArray)
+
+  },[repos])
+
+  console.log(mergerepos)  
 
     return (
       <main className={styles.main} >
@@ -35,9 +50,12 @@ const [isVisible2, setIsVisible2] = useState(false)
           <div className={`${styles.projectState} ${isVisible ? styles.visible  : styles.hidden}`}>
             <h2 className={styles.title}>{language == 'es' ?("En desarrollo"):("Working on")}</h2>                
               {isVisible && (
-                <div className={styles.projectCards}>
-                  {repos.map((e)=>(
-                    <ProjectCard key={e.id} Project={e}/>
+                <div className={styles.projectCards} >
+                  {mergerepos.map((e)=>(
+                   <div className={styles.cardRender} key={e.id}>
+                    {e.finalized ? (<ProjectCard key={e.id} Project={e}/>) : null}
+                  </div>
+                    
                   ))}
                 </div> 
               )}
@@ -53,9 +71,12 @@ const [isVisible2, setIsVisible2] = useState(false)
             <h2 className={styles.title}>{language == 'es' ?("Finalizados"):("finalized")}</h2>           
               {isVisible2 && (
                 <div className={styles.projectCards}>
-                  {repos.map((e)=>(
-                    <ProjectCard key={e.id} Project={e}/>
-                  ))} 
+                  {mergerepos.map((e)=>(
+                   <div key={e.id}>
+                    {e.finalized ?  null : (<ProjectCard key={e.id} project={e}/>)}
+                  </div>
+                    
+                  ))}
                 </div> 
               )}
             <button 
